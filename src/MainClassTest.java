0f35de003badfbfbc65d1a56456cefae0d34dcf8
ameliksetyan <a.melixetyan@yandex.ru>
2024-02-08 15:37:59 +0300

    Homework #2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -8,4 +8,11 @@ public class MainClassTest {
         int result = mainClass.getLocalNumber();
         Assertions.assertEquals(14, result, "метод getLocalNumber возвращает число 14");
     }
+
+    @Test
+    public void testGetClassNumber() {
+        MainClass mainClass = new MainClass();
+        int result = mainClass.getClassNumber();
+        Assertions.assertTrue(result > 45, "метод getClassNumber возвращает число больше 45");
+    }
 }
